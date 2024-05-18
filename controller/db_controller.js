daf10494e4912c9caae9b2cc47a26c3bb075f079
ngarcia <garcia.noav@gmail.com>
2024-05-17 21:30:59 -1000

    feat: added mongo packs, simple get from db

--- controller/db_controller.js
+++ controller/db_controller.js
@@ -4,3 +4,28 @@
  * information.
  */
 
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.5"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+tasks = NONE;
+
+function get_data() {
+    try {
+        client.connect();
+        const database = client.db('todoList');
+        const collection = database.collection('todoTasks');
+
+        const cursor = collection.find();
+
+        // convert to array and log the documents
+        const tasks = cursor.toArray();
+        console.log(tasks);
+
+    } catch(error) {
+        console.error("Error loading documents from database:: ", error);
+    } finally {
+        client.close();
+    }
+    return tasks;
+}
